933542adda6c65dd9b69c01bafa9f73c28ef882c
William Monk <will@williammonk.co.uk>
2020-06-12 10:20:47 +0100

    Add Github

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const core = require('@actions/core');
 const wait = require('./wait');
+const github = require("@actions/github")
 
 
 // most @actions toolkit packages have async methods
 async function run() {
+
+  console.log(github.context)
+
   try { 
     const ms = core.getInput('milliseconds');
     console.log(`Waiting ${ms} milliseconds ...`)
